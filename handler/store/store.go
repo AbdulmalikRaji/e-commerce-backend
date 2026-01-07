c656294e827736b9da312fd77f8f43376b9e1fd5
Abdulmalik Raji <rajimalik2002@gmail.com>
2026-01-07 18:28:56 +0100

    add store handler functions

--- handler/store/store.go
+++ handler/store/store.go
@@ -1,7 +1,9 @@
 package store
 
 import (
+	"github.com/abdulmalikraji/e-commerce/dto/storeDto"
 	"github.com/abdulmalikraji/e-commerce/services"
+	"github.com/abdulmalikraji/e-commerce/utils/genericResponse"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -21,9 +23,30 @@ func New(service services.StoreService) StoreHandler {
 }
 
 func (c storeHandler) CreateStore(ctx *fiber.Ctx) error {
-	return nil
+	var request storeDto.CreateStoreRequest
+	if err := ctx.BodyParser(&request); err != nil {
+		return err
+	}
+
+	statusCode, err := c.service.CreateStore(ctx, request)
+	if err != nil {
+		return genericResponse.ErrorResponse(ctx, fiber.StatusBadRequest, err.Error())
+	}
+
+	return genericResponse.SuccessResponse(ctx, statusCode, nil, "Store created successfully")
 }
 
 func (c storeHandler) GetStoreByID(ctx *fiber.Ctx) error {
-	return nil
+	var request storeDto.GetStoreByIDRequest
+	if err := ctx.QueryParser(&request); err != nil {
+		return err
+	}
+
+	response, statusCode, err := c.service.GetStoreByID(ctx, request)
+	if err != nil {
+		return genericResponse.ErrorResponse(ctx, fiber.StatusBadRequest, err.Error())
+
+	}
+
+	return genericResponse.SuccessResponse(ctx, statusCode, response, "Store retrieved successfully")
 }
